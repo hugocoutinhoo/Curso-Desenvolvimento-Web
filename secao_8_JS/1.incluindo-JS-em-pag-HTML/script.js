22eae71d4877773e8ed25328e4248a56431e7428
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-09 15:02:21 -0300

    início do curso JS

---
+++ secao_8_JS/1.incluindo-JS-em-pag-HTML/script.js
@@ -0,0 +1,7 @@
+alert("Olá! Seja bem vindo ao curso.") //alerta que irá disparar quando a pag for carregada
+
+/*
+    selecionar um elemento no DOM
+    atualizar o valor desse elemento com uma string
+*/
+document.getElementById('nome').value = "Hugo"
