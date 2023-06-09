1a7a8fd56cb763d64a6d979ad76abf37b515831b
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-09 16:30:54 -0300

    variáveis

---
+++ secao_8_JS/2.variaveis/script.js
@@ -0,0 +1,19 @@
+// variável string
+var nome = "Hugo";
+
+//variáveis number
+var numInt = -9;
+var numFlo = 10.75;
+
+//variáveis boolean
+var teste = true;
+var teste2 = false;
+
+console.log(typeof(nome))
+console.log(typeof(numInt))
+console.log(typeof(numFlo))
+console.log(typeof(nome))
+console.log(typeof(nome))
+
+//escrever valores dentro do browser
+document.write(nome)
