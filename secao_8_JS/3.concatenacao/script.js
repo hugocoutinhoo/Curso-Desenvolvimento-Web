a9ca56b51de696f87876a31497037a8c898a6a63
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-09 16:42:35 -0300

    concatenação

---
+++ secao_8_JS/3.concatenacao/script.js
@@ -0,0 +1,14 @@
+/*
+var nome = "Hugo";
+var idade = 29;
+var nome2 = "Mirelly"
+var idade2 = 26;
+document.write(`<h1>Olá ${nome}, tudo bem? Vi que você tem ${idade} anos.</h1>`);
+document.write(`<h1>Olá ${nome2}, tudo bem? Vi que você tem ${idade2} anos.</h1>`);
+*/
+
+var nome = prompt("Digite seu nome: ");
+
+document.write(`<h2>Olá ${nome}, tudo bem?</h2>`);
+
+//concatenação
