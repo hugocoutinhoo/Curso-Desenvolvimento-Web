1cf36295aecc70293a9cfad6cee1d01dba07ff67
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-09 16:52:58 -0300

    variáveis

--- secao_8_JS/2.variaveis/script.js
+++ secao_8_JS/2.variaveis/script.js
@@ -6,14 +6,20 @@ var numInt = -9;
 var numFlo = 10.75;
 
 //variáveis boolean
-var teste = true;
-var teste2 = false;
+//var teste = true;
+//var teste2 = false;
+
+//variáveis null
+var teste = null
+
+//varáveis undefined
+var teste2
 
 console.log(typeof(nome))
 console.log(typeof(numInt))
 console.log(typeof(numFlo))
-console.log(typeof(nome))
-console.log(typeof(nome))
+console.log(typeof(teste))
+console.log(typeof(teste2))
 
 //escrever valores dentro do browser
-document.write(nome)
+//document.write(nome)
