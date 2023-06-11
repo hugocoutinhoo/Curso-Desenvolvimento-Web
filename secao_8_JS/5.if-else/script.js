71bfe6f8838c48014837a5ef528157a199eaf8ce
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-11 07:07:55 -0300

    if-else part1

---
+++ secao_8_JS/5.if-else/script.js
@@ -0,0 +1,19 @@
+//document.write('<h1>Javascript - If/else</h1>');
+
+// ----------- Praticando com operadores aritméticos
+
+/*
+var nota1 = 7;
+var nota2 = 8;
+var media = (nota1 + nota2) / 2
+
+
+if (media >= 7) {
+    console.log('Parabéns, você foi aprovado.')  
+} else {
+    console.log('Infelizmente você não foi aprovado.')   
+}
+*/
+
+// ------------ Praticando com operadores lógicos
+
