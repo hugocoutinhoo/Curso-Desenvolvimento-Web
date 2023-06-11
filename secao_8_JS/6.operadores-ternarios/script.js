cd11a69f9bbb0d067334498049e28613e67dcc5a
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-11 07:24:41 -0300

    operador ternário

---
+++ secao_8_JS/6.operadores-ternarios/script.js
@@ -0,0 +1,10 @@
+
+
+var nota = 7
+var media = 7
+var faltas = 5
+var faltas_maximas = 8
+
+var resultado = (nota >= media && faltas <= faltas_maximas) ? 'aprovado' : 'reprovado'
+
+console.log(resultado)
