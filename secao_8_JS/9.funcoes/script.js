085a3c79939956bf875317dae6c05289ff1aef9a
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-12 15:51:13 -0300

    funções pt 1

---
+++ secao_8_JS/9.funcoes/script.js
@@ -0,0 +1,30 @@
+
+
+
+
+/* function calcularAreaTerreno (largura, comprimento) {
+    //lógica
+    var area = largura * comprimento
+
+    return area
+}
+
+// console.log(`O terreno possui uma área de ${calcularAreaTerreno(15, 25)} metros quadrados.`)
+
+var largura = Number(prompt('Digite a largura do terreno em metros: '))
+var comprimento = Number(prompt('Digite o comprimento do terreno em metros: '))
+
+var area = calcularAreaTerreno(largura, comprimento)
+
+document.write(`O terreno possui uma área de ${area} metros quadrados.`)
+
+// console.log(`O terreno possui uma área de ${area} metros quadrados.`)
+
+*/
+
+function soma(a, b, c) {
+    return a + b + c
+}
+
+console.log(soma(7, 7)) // retorna um NaN
+console.log(soma(7, 7, 5, 9)) // desconsidera os parâmetros adicionais
