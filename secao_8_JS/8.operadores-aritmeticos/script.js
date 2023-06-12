79e03d20e0237e884878d1d65945461c93a68ef2
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-12 15:01:41 -0300

    operadores aritméticos

---
+++ secao_8_JS/8.operadores-aritmeticos/script.js
@@ -0,0 +1,23 @@
+var num1 = 9
+var num2 = 2
+
+//precedência -> () 
+console.log(`A soma entre ${num1} + ${num2} é: ` + (num1 + num2)) 
+console.log(`A subtração entre ${num1} + ${num2} é: ` + (num1 - num2)) 
+console.log(`A multiplicação entre ${num1} + ${num2} é: ` + (num1 * num2)) 
+console.log(`A divisão entre ${num1} + ${num2} é: ` + (num1 / num2)) 
+console.log(`O módulo (resto da divisão) entre ${num1} + ${num2} é: ` + (num1 % num2)) 
+console.log(`O incremento de ${num1} é: ` + (++num1)) 
+console.log(`O decremento de ${num1} é: ` + (--num1))
+
+// atribuição de valores
+
+var teste = 10
+
+// teste += 5
+// teste -= 5
+// teste *= 5
+// teste /= 5
+// teste %= 5
+// teste **= 5
+console.log(teste)
