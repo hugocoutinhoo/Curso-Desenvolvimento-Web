112671a131b0785398a5c31389aa8b1655b955f5
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-12 17:43:22 -0300

    funções pt3

--- secao_8_JS/9.funcoes/5.funcoes-nativas/script.js
+++ secao_8_JS/9.funcoes/5.funcoes-nativas/script.js
@@ -1,7 +1,6 @@
 // Funções nativas para manipular strings
 
-// * atributo -> length
-
+/*
 var nome = 'Jorge Sant Ana'
 console.log(nome.length) // 14
 console.log(nome.charAt(4)) // e
@@ -10,3 +9,22 @@ console.log(nome.replace('Sant Ana', 'Silva')) // Jorge Silva
 console.log(nome.toLowerCase()) // jorge sant ana
 console.log(nome.toUpperCase()) // JORGE SANT ANA
 console.log('-' + nome.trim() + '-') // -Jorge Sant Ana-
+*/
+
+// Funções nativas para tarefas matemáticas
+
+/*
+var x = 10.380
+ // 11
+
+console.log(Math.ceil(x)) // 11
+console.log(Math.floor(x)) // 10
+console.log(Math.random(x)) // número randômico
+*/
+
+// Funções nativas para manipular datas
+var data = new Date()
+
+console.log(data.getDate())
+console.log(data.getMonth() + 1)
+console.log(data.getFullYear())
