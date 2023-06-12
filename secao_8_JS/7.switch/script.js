41b0495a7ca219f25eafe83da8cdbd81368ce6f7
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-12 14:35:14 -0300

    switch

---
+++ secao_8_JS/7.switch/script.js
@@ -0,0 +1,17 @@
+
+var parametro = 2
+
+
+switch (parametro) {
+
+    case 1:
+        document.write('Parametro 1')
+        break
+
+    case 2:
+        document.write('Parametro 2')
+        break
+
+    default:  //opcional
+        document.write('default')
+}
