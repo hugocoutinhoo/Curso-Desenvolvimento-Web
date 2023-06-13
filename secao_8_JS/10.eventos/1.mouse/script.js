a5640d37f92190d746aaded1f6bfaeb8a13c6cee
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-13 14:14:59 -0300

    eventos de mouse

---
+++ secao_8_JS/10.eventos/1.mouse/script.js
@@ -0,0 +1,15 @@
+/*
+onclick -> acionado quando o elemento é clicado.
+ondblclick -> acionado quando o elemento é clicado 2x.
+onmouseup -> acionado quando o click sobre determinado elemento é liberado.
+onmouseover -> acionado quando o cursor do mouse sobrepõe o elemento.
+onmouseout -> acionado quando o cursor sai da página ocupada pelo elemento.
+*/
+
+function acao1() {
+    alert('Elemento foi clicado')
+}
+
+function acao2() {
+    alert('O cursor do mouse deixou a região do elemento')
+}
