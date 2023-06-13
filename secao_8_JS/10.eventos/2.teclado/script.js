1cf6d9254ebf69796c11507a246dabd79371ff91
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-13 14:21:18 -0300

    eventos teclado

---
+++ secao_8_JS/10.eventos/2.teclado/script.js
@@ -0,0 +1,9 @@
+/*
+onkeydown -> acionado no momento em que uma tecla é pressionada
+onkeyup -> acionado no momento em que uma tecla é liberada
+onkeypress -> acionado quando mantenho a tecla pressionada
+*/
+
+function acao1() {
+    alert('Evento disparado')
+}
