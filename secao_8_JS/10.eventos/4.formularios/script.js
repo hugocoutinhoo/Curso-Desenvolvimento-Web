c470139479c22908e49d880750429f5cf0584978
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-13 14:35:15 -0300

    evento em formulário

---
+++ secao_8_JS/10.eventos/4.formularios/script.js
@@ -0,0 +1,5 @@
+/*
+onfocus -> acionado quando o elemento recebe o foco do cursor do mouse.
+onblur -> acionado quando o elemento perde o foco.
+onchange -> acionado quando o estado do elemento é modificado
+*/
