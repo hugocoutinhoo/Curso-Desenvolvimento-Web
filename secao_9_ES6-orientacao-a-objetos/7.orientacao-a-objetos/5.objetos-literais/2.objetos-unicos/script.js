aa953cb4f4d2fbe413436e223653209db25efb50
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-16 17:19:07 -0300

    objetos únicos

---
+++ secao_9_ES6-orientacao-a-objetos/7.orientacao-a-objetos/5.objetos-literais/2.objetos-unicos/script.js
@@ -0,0 +1,17 @@
+let assinatura = {
+    idCliente: 1000,
+    descricao: 'Acesso a internet',
+    status() {
+        console.log('Ativo')
+    }
+}
+
+console.log(assinatura.descricao)
+
+let y = assinatura
+
+console.log(y.descricao)
+
+y.descricao = 'Internet + tv + telefone'
+console.log(assinatura.descricao)
+console.log(y.descricao)
