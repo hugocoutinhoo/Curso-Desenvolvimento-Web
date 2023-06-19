7308de3e825f38f361cda0f8287baa0d7025803c
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-19 18:29:32 -0300

    aula sobre spread

--- secao_9_ES6-orientacao-a-objetos/9.prototype/heranca/script.js
+++ secao_9_ES6-orientacao-a-objetos/9.prototype/heranca/script.js
@@ -1,12 +1,17 @@
 
-//Object.prototype
+Object.prototype.atributo50 = 'z' //método global
+
 let animal = {
     atributo1: 'a'
 }
 let vertebrado = {
+    __proto__: animal,
     atributo2: 'b'
 }
 let ave = {
-    __proto__
-    atributo3: 'c'
+    __proto__: vertebrado,
+    atributo3: 'c',
+    atributo2: 'x'
 }
+
+console.log(ave.atributo3, ave.atributo2, ave.atributo1, ave.atributo50)
