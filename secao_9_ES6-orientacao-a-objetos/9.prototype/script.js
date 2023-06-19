90caaf4755d850bb1410c89195e1536fda69681c
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-06-19 18:04:52 -0300

    prototype pt1

---
+++ secao_9_ES6-orientacao-a-objetos/9.prototype/script.js
@@ -0,0 +1,37 @@
+// objeto literal
+let a1 = {
+    cor: 'branco',
+    modelo: 'Airbus a380',
+    levantarVoo: function() {
+        console.log('Levantar voo')
+    }
+}
+
+// função construtora
+let AviaoF = function() {
+    this.cor = 'azul',
+    this.modelo = 'Boeing 787',
+    this.levantarVoo = function() {
+        console.log('Levantar voo')
+    }
+}
+
+let a2 = new AviaoF()
+
+// classe
+class AviaoC {
+    constructor() {
+        this.cor = 'vermelho',
+        this.modelo = 'Embraer E-Jets'
+    }
+
+    levantarVoo() {
+        console.log('Levantar voo')
+    }
+}
+
+let a3 = new AviaoC()
+
+console.log(a1)
+console.log(a2)
+console.log(a3)
