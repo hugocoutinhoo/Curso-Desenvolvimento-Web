60e18c80a0a7173eae4c5d2e2e276a64c75702d0
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-07-12 13:04:38 -0300

    php com pdo pt3

--- secao_15_PHP_com_PDO_MySQL/php_com_pdo/index.php
+++ secao_15_PHP_com_PDO_MySQL/php_com_pdo/index.php
@@ -1,5 +1,8 @@
 <?php
 
+    if(!empty($_POST['usuario']) && !empty($_POST['senha'])) {
+
+
     $dsn = 'mysql:host=localhost;dbname=php_com_pdo';
     $usuario = 'root';
     $senha = '';
@@ -32,18 +35,146 @@
                 )
         ';
         */
-
+        /*
         $query = '
                 delete from tb_usuarios
         ';
 
         $retorno = $conexao->exec($query);
         echo ($retorno);
+        */
+        /*
+        $query = '
+                insert into tb_usuarios(
+                    nome, email, senha
+                ) values (
+                    "Jorge Sant Ana", "jorge@teste.com.br", "123456"
+                )
+        ';
+
+        $conexao->exec($query);
+
+        $query = '
+                insert into tb_usuarios(
+                    nome, email, senha
+                ) values (
+                    "Jamilton Damasceno", "jamilton@teste.com.br", "456789"
+                )
+        ';
+
+        $conexao->exec($query);
+
+        $query = '
+                insert into tb_usuarios(
+                    nome, email, senha
+                ) values (
+                    "Maria Silva", "maria@teste.com.br", "123456"
+                )
+        ';
+
+        $conexao->exec($query);
+        */
+
+        //----------------Selecionando vários registros -------------------
+        /*
+        $query = '
+            select * from tb_usuarios
+        ';
+
+        $stmt = $conexao->query($query);
+        $lista = $stmt->fetchAll(PDO::FETCH_BOTH);
+        //se o retorno for por OBJ, deve alterar a forma de acessar os valores 
+
+        echo '<pre>';
+        print_r($lista);
+
+        echo $lista[2]['email'];
+        */
+
+        //----------------Selecionando apenas um registro ------------------
+        /*
+        $query = '
+            select * from tb_usuarios order by nome desc limit 1
+        ';
+
+        $stmt = $conexao->query($query);
+        $usuario = $stmt->fetch(PDO::FETCH_OBJ);
+        //se o retorno for por OBJ, deve alterar a forma de acessar os valores 
+
+        echo '<pre>';
+        print_r($usuario);
+
+        echo $usuario->nome;
+        */
+
+        /*
+        $query = '
+            select * from tb_usuarios 
+        ';
+        //------------- Utilizando o FOREACH para listar registros
+        //$stmt = $conexao->query($query);
+
+        foreach($conexao->query($query) as $key => $value) {
+            print_r($value);
+            echo '<hr>';
+        }
+        */
+        //$lista_usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        //se o retorno for por OBJ, deve alterar a forma de acessar os valores 
+
+        //echo '<pre>';
+        //print_r($lista_usuarios);
+
+        /*
+        foreach($lista_usuarios as $key => $value) {
+            echo $value['nome'];
+            echo '<hr>';
+        }
+        */
+
+        $query = "select * from tb_usuarios where ";
+        $query .= " email = :usuario ";
+        $query .= " AND senha = :senha ";
+
+        $stmt = $conexao->prepare($query);
+
+        $stmt->bindValue('usuario', $_POST['usuario']);
+        $stmt->bindValue('senha', $_POST['senha'], PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        $usuario = $stmt->fetch();
+
+        echo '<hr>';
+        echo '<pre>';
+        print_r($usuario);
+        
+
+        
 
     } catch(PDOException $e) {
         echo 'Erro: ' .$e->getCode().' Mensagem: ' .$e->getMessage();
         //registrar erro
     }
+}
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        <input type="text" name="usuario" id="" placeholder="usuário">
+        <br><br>
+        <input type="password" name="senha" id="" placeholder="senha">
+        <br><br>
+        <button type="submit">Entrar</button>
+    </form>
+</body>
+</html>
